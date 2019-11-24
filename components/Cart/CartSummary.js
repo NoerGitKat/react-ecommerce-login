bbe8b5fc6acf1d2e441efd83b10051fb798d20bc
Noer Paanakker <noer@hackyourfuture.net>
2019-11-24 18:57:42 +0100

    created cart price calculation helper + put in cart component state

--- components/Cart/CartSummary.js
+++ components/Cart/CartSummary.js
@@ -1,18 +1,24 @@
 import { useState, useEffect } from "react";
 import { Button, Segment, Divider } from "semantic-ui-react";
+import calculateCartTotal from "./../../utils/calculateCartTotal";
 
 function CartSummary({ products }) {
+  const [cartAmount, setCartAmount] = useState(0);
+  const [stripeAmount, setStripeAmount] = useState(0);
   const [isCartEmpty, setIsCartEmpty] = useState(false);
 
   useEffect(() => {
     setIsCartEmpty(products.length === 0);
+    const { cartTotal, stripeTotal } = calculateCartTotal(products);
+    setCartAmount(cartTotal);
+    setStripeAmount(stripeTotal);
   }, [products]);
 
   return (
     <>
       <Divider />
       <Segment clearing size="large">
-        <strong>Sub total:</strong> $0.00
+        <strong>Sub total:</strong> ${cartAmount}
         <Button
           icon="cart"
           color="teal"
--- utils/calculateCartTotal.js
+++ utils/calculateCartTotal.js
@@ -1,8 +1,14 @@
 const calculateCartTotal = products => {
-  products.reduce((accumulator, element) => {
-    accumulator += element.product.price * element.quantity;
-    return accumulator;
+  const total = products.reduce((totalResult, currentVal) => {
+    totalResult += currentVal.product.price * currentVal.quantity;
+    return totalResult;
   }, 0);
+  const cartTotal = ((total * 100) / 100).toFixed(2);
+  const stripeTotal = Number((total * 100).toFixed(2));
+  return {
+    cartTotal,
+    stripeTotal
+  };
 };
 
 export default calculateCartTotal;
